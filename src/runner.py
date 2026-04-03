073fe76704f54185860e328af69a0db96af46177
suppo01 <143649571+suppo01@users.noreply.github.com>
2026-04-02 21:55:33 -0400

    Add main function for WinClean performance runner
    Implement main function to run WinClean experiments

--- src/runner.py
+++ src/runner.py
@@ -1,1 +1,30 @@
+def main():
+  """Runner for experiments regarding the performance of WinClean"""
+  # Initialize the counter for passes
+  pass_counter = 0
+  # Read in the input into a list
 
+  # Loop through the inputs
+  for path in paths:
+    # Run the input through WinClean
+    winclean_output = ""
+    # Increment the counter after the pass through WinClean
+    pass_counter += 1
+    # Run the result from WinClean in a Venv and collect any errors
+    venv_errors = ""
+    # Check for the number of errors
+    if venv_errors is None:
+      # Print output to terminal
+      print(f"Here is the solution offered by WinClean: /n/n{winclean_output}")
+      # Print final counter to terminal
+      plural = ""
+      if pass_counter > 1:
+        plural = "es"
+      print(f"The input was cleaned in {pass_counter} pass{plural}")
+      # Write the input, the number of passes, and the types of bugs to the csv file
+      data_to_csv = ""
+    else:
+      # Print attempt to terminal
+      print(f"Here is the attempted solution offered by WinClean: /n/n{winclean_output} /n/nTrying Again...")
+      # Send the failed attempt back through WinClean and try again
+      winclean_output = ""
